c192f2f705f9be61f02f991646005d7937e46d4d
Jose Luis Retis <joseluisretis@gmail.com>
2021-01-28 23:28:54 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid';
+
+export default function GifExpertApp() {
+
+    const [categories, setCategories] = useState(['the rock']);
+    
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr />
+
+            <AddCategory setCategories={ setCategories } />
+
+            <ol>
+                {
+                    categories.map( category => (
+                       <GifGrid 
+                            key={ category }
+                            category={ category } />
+                    ))
+                }
+            </ol>
+        </>
+    )
+}
